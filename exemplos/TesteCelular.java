e183db8fa9062eadddc66e1c4e1f8ce2bddcd24b
Rafael Oliveira <rafael.infopiaui@gmail.com>
2026-02-24 18:08:29 -0300

    docs: atualizar teste de celular

--- exemplos/TesteCelular.java
+++ exemplos/TesteCelular.java
@@ -1,13 +1,13 @@
 public class TesteCelular {
     public static void main(String[] args) {
-        // Instanciação
+        // 1. Instanciação: Criando o objeto na memória RAM usando 'new'
         Celular meuaparelho = new Celular();
         
-        // Atribuições
+        // 2. Atribuições: Definindo o estado do objeto
         meuaparelho.marca = "Samsung";
         meuaparelho.bateria = 85;
 
-        //Execução
+        //3. Execução: Enviando uma mensagem ao objeto (chamando o metodo)
         System.out.println("Iniciando o teste no sistema: ");
         meuaparelho.mostrarStatus();
     }
